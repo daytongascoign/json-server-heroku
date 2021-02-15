75debb28cf3ce5c45cc17bfee593980fe8c433e5
john <johnnn1591@gmail.com>
2021-02-15 13:58:45 +0700

    add multi network

--- index.js
+++ index.js
@@ -4,36 +4,55 @@ const fs = require('fs')
 
 const Web3 = require('web3')
 
-
-
-const NFT_FACTORY_ADDRESS = '0xE10a109218D0e7c258A49f69BC7337193f28e815';
-const NFT_ADDRESS = '0x1c137Ba270fbcD22D3c7Bdac101f0A21cb0885cE';
-const HTTP_NETWORK = 'https://http-mainnet-node.huobichain.com';
+var CONTRACT_ADDRESS = {
+    NFT_FACTORY_ADDRESS : '0xE10a109218D0e7c258A49f69BC7337193f28e815',
+    NFT_ADDRESS : '0x1c137Ba270fbcD22D3c7Bdac101f0A21cb0885cE',
+    HTTP_NETWORK : 'https://http-mainnet-node.huobichain.com'
+}
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
-app.get('/collection', function(req,res){
-    res.header("Access-Control-Allow-Origin", "*");
 
-    var s = JSON.parse(fs.readFileSync('collection.json'));
+function GetContractById(chainId){
+    switch(chainId){
+        case 56 :
+            break;
+        case 97 :
+            CONTRACT_ADDRESS = {
+                NFT_FACTORY_ADDRESS : '0x154dfB0a0e3E81f0659ACB2973b185735dF88b10',
+                NFT_ADDRESS : '0xAA990C25732906b87B1fF3934a76505Fc0991608',
+                HTTP_NETWORK : 'https://data-seed-prebsc-1-s1.binance.org:8545/'
+            }
+            break;
+        case 128 : 
+                CONTRACT_ADDRESS = {
+                    NFT_FACTORY_ADDRESS : '0xE10a109218D0e7c258A49f69BC7337193f28e815',
+                    NFT_ADDRESS : '0x1c137Ba270fbcD22D3c7Bdac101f0A21cb0885cE',
+                    HTTP_NETWORK : 'https://http-mainnet-node.huobichain.com'
+                }
+            break;
+        case 256 : 
+            break;
+    }
+}
 
-    res.send(s);
-});
-app.get('/null-card/:id', function(req,res){
 
+app.get('/null-card/:id/:chainId', function(req,res){
     res.header("Access-Control-Allow-Origin", "*");
 
     var cardAvaiables = JSON.parse(fs.readFileSync('null-cards.json'));
     var collections = JSON.parse(fs.readFileSync('collection.json'));
     var result = cardAvaiables.find(x => x.id == req.params.id);
     if (result == "" || result == null || result == undefined) {
-        var web3 = new Web3(HTTP_NETWORK);
+        GetContractById(req.params.chainId);
+        console.log(CONTRACT_ADDRESS);
+        var web3 = new Web3(CONTRACT_ADDRESS.HTTP_NETWORK);
         var nft_abi = JSON.parse(fs.readFileSync(`nft.json`));
         var nft_factory_abi = JSON.parse(fs.readFileSync(`nft-factory.json`));
-        var contract = new web3.eth.Contract(nft_abi, NFT_ADDRESS);
-        var contract_factory = new web3.eth.Contract(nft_factory_abi, NFT_FACTORY_ADDRESS);
+        var contract = new web3.eth.Contract(nft_abi, CONTRACT_ADDRESS.NFT_ADDRESS);
+        var contract_factory = new web3.eth.Contract(nft_factory_abi, CONTRACT_ADDRESS.NFT_FACTORY_ADDRESS);
         let lst = [];
         let cardId  = req.params.id;
         if (cardAvaiables.find(abi => abi.id == cardId) == undefined) {
@@ -64,18 +83,9 @@ app.get('/null-card/:id', function(req,res){
                         }
                         cardAvaiables.push(obj);
                         let data = JSON.stringify(cardAvaiables);
-                        console.log(cardAvaiables);
                         fs.writeFileSync('null-cards.json', data);
-
-                        req.send(obj)
                     })
-
-                   
-
-                   
                 })
-
-
         }
     }
     result = cardAvaiables.find(x => x.id == req.params.id);
@@ -89,11 +99,13 @@ app.get('/hecochain', function(req,res){
     res.send(s);
 });
 
+app.get('/collection', function(req,res){
+    res.header("Access-Control-Allow-Origin", "*");
 
+    var s = JSON.parse(fs.readFileSync('collection.json'));
 
-
-
-
+    res.send(s);
+});
 
 
 app.listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
